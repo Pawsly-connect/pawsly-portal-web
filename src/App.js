417f015e1b5efd910014434b746d5b6d648b10ff
Richard suan <richard_98@live.com>
2024-04-06 17:37:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Registro from "./components/SignUp/SignUp";
+//import Registro from "./components/SignUp/SignUp";
 import Ingreso from "./components/LogIn/LogIn"
 function App() {
   return (
