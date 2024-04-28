fea8cf0340b1febc1732c9779200563539ab56e4
David Garcia <e.davidgv@hotmail.com>
2024-04-27 23:25:20 -0500

    change url

--- src/service/authMngr/index.js
+++ src/service/authMngr/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create();
 
-instance.defaults.baseURL = "https://1vd78290pl.execute-api.us-east-1.amazonaws.com";
+instance.defaults.baseURL = "https://1vd78290pl.execute-api.us-east-1.amazonaws.com/auth-mngr-stg";
 instance.defaults.headers.common["X-name"] = "Portal";
 instance.defaults.headers.common["X-api-key"] = "";
 
