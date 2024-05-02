e27e91c92c1f3c7aaa84ccc8d5c7baafe05fa22a
David Garcia <e.davidgv@hotmail.com>
2024-05-01 22:26:43 -0500

    resolve conflicts

--- src/service/authMngr/index.js
+++ src/service/authMngr/index.js
@@ -2,5 +2,6 @@ import instance from "../index";
 
 instance.defaults.baseURL = "https://d1i75zkrg41h7t.cloudfront.net";
 instance.defaults.headers.common["X-name"] = "Portal";
+instance.defaults.headers.common["Content-type"] = "text/plain"
 
 export default instance;
